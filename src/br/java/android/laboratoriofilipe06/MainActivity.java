909c1e47185ac3fdbc2625ebdca4be480ebb0bfa
FilipeVercozaAlve <filipevercoz@outlook.com>
2015-06-21 17:12:47 -0300

    laboratorio06

---
+++ src/br/java/android/laboratoriofilipe06/MainActivity.java
@@ -0,0 +1,42 @@
+/***
+* @author Filipe Verçoza
+*/
+package br.java.android.laboratoriofilipe06;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected <Gallery> void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+       Gallery minhaGaleria = (Gallery)findViewById(R.id.galeriaImagem);
+        minhaGaleria.setAdapter(new AdaptadorImagens(this));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
